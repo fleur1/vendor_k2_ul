e1de86b1fe0cb6d8daf0b5f8bf492debf0478d04
vl197602 <radarga@gmail.com>
2013-10-09 09:10:46 +0400

    Update k2_ul-vendor-blobs.mk

--- k2_ul-vendor-blobs.mk
+++ k2_ul-vendor-blobs.mk
@@ -37,10 +37,7 @@ vendor/htc/vendor_onesv/proprietary/lib/hw/sensors.msm8960.so:system/lib/hw/sens
   vendor/htc/vendor_onesv/proprietary/bin/thermald:system/bin/thermald \
   vendor/htc/vendor_onesv/proprietary/lib/libacdbloader.so:system/lib/libacdbloader.so \
   vendor/htc/vendor_onesv/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
-vendor/htc/vendor_onesv/proprietary/lib/libaudioflinger.so:system/lib/libaudioflinger.so \
   vendor/htc/vendor_onesv/proprietary/lib/libalsa-intf.so:system/lib/libalsa-intf.so \
-# vendor/htc/vendor_onesv/proprietary/lib/libOmxVdec.so:system/lib/libOmxVdec.so \
-# vendor/htc/vendor_onesv/proprietary/lib/libOmxVenc.so:system/lib/libOmxVenc.so \
   vendor/htc/vendor_onesv/proprietary/lib/libaudioalsa.so:system/lib/libaudioalsa.so \
   vendor/htc/vendor_onesv/proprietary/lib/libacdbmapper.so:system/lib/libacdbmapper.so \
   vendor/htc/vendor_onesv/proprietary/lib/libcameraface.so:system/lib/libcameraface.so \
